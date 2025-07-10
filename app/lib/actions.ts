d82703574231bd346cd77a2d73b2ecf0427a7995
Sacha <150120887+c-est-sa@users.noreply.github.com>
2025-07-11 03:14:07 +0900

    chapter 13 - handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,15 +29,20 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100; // To avoid JavaScript floating-point errors
   const date = new Date().toISOString().split("T")[0]; // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error("Error creating invoice:", error);
+  }
 
   // Revalidate clears the cache and trigger a new request to the server to fetch the latest data.
   // Once the database has been updated, the /dashboard/invoices path will be revalidated.
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+  // redirect would only be reachable if try is successful.
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
@@ -49,17 +54,23 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    console.error("Error updating invoice:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+  // throw new Error("Failed to Delete Invoice");
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
